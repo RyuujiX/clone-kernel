4b41c01c30b32e25eb240821ff7eb859b8714f9a
RyuujiX <saputradenny712@gmail.com>
2021-02-27 21:16:00 +0700

    Update Repo
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 KERNEL=$(pwd)/kernel-dir
-git clone https://github.com/RyuujiX/kernel-xobod-r -b lineage-18.1-source $KERNEL
+git clone https://$GIT_SECRET@github.com/$GIT_USERNAME/kernel-xobod-r -b lineage-18.1-source $KERNEL
 cd $KERNEL
-git remote add myrepo https://github.com/RyuujiX/kernel-xobod-los18
+git remote add myrepo https://$GIT_SECRET@github.com/$GIT_USERNAME/kernel-xobod-los18
 git push -f myrepo --all
 cd ..
 rm -rf $KERNEL
