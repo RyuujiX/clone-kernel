8df88236ca37378e1dc49ea06361d163bdaf143c
RyuujiX <saputradenny712@gmail.com>
2021-07-30 04:38:09 +0700

    Update Source Link

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 KERNEL=$(pwd)/kernel-dir
-git clone https://github.com/Vivekachooz/android_kernel_asus_sdm660 -b lineage-17.1 $KERNEL
+git clone https://github.com/Atom-X-Devs/android_kernel_asus_sdm660 -b dot11 $KERNEL
 cd $KERNEL
-git remote add myrepo https://$GIT_SECRET@github.com/$GIT_USERNAME/kernel-x00t
+git remote add myrepo https://$GIT_SECRET@github.com/$GIT_USERNAME/android_kernel_asus_sdm660_4.19
 git push -f myrepo --all
 cd ..
 rm -rf $KERNEL
