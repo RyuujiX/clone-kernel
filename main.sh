dbfd43937cfce98d1c61bb0c24fc10460dc7c87d
RyuujiX <saputradenny712@gmail.com>
2021-08-20 06:49:27 +0700

    Private Source
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 KERNEL=$(pwd)/kernel-dir
 git clone https://github.com/Atom-X-Devs/atom-x-tc-build -b main $KERNEL
 cd $KERNEL
-git remote add myrepo https://github.com/RyuujiX/tc-builder
+git remote add myrepo https://$GIT_SECRET@github.com/$GIT_USERNAME/tc-builder
 git push -f myrepo --all
 cd ..
 rm -rf $KERNEL
